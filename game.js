2abb0e5c2e74cae032a9f83fa76fa88c599792e0
driverwolf <driverwolf@gmail.com>
2021-01-26 23:28:03 +0200

    changed __proto__.contructor.name to instanceof and write function fight(enemy) in Fighter

--- game.js
+++ game.js
@@ -26,56 +26,48 @@ class Fighter {
     isAlive() {
         return this.hitpoints > 0;
     }
-}
-class Chempion extends Fighter {
-    d = false;
-    constructor(obj) {
-        super(obj);
-    }
     fight(enemy) {
         if (enemy === this) return "Ви б'єте самого себе";
 
-        if (enemy.d && enemy.__proto__.constructor.name === 'Chempion') {
+        if (enemy.defenceValue && enemy instanceof Chempion) {
             enemy.totalHitpoints++;
-            enemy.d = false;
+            enemy.defenceValue = false;
             return 'Ваш опонент чемпіон і він блокував атаку';
         }
+        if (this instanceof Chempion) {
+            if (enemy.isAlive() && this.isAlive()) enemy.hitpoints -= this.attack;
+            else return 'Ваш опонент мертвий або ви';
 
-        if (enemy.isAlive() && this.isAlive()) enemy.hitpoints -= this.attack;
-        else return 'Ваш опонент мертвий або ви';
+            this.attack = enemy.isAlive() ? this.attack : ++this.attack;
+        } else if (this instanceof Monster) {
+            if (enemy.isAlive() && this.isAlive()) {
+                this.burst === 2 || this.burst === 1 ? (enemy.hitpoints -= this.attack * 2) : (enemy.hitpoints -= this.attack);
+                this.burst--;
+            } else return 'Ваш опонент мертвий або ви';
 
-        this.attack = enemy.isAlive() ? this.attack : ++this.attack;
+            if (!enemy.isAlive()) {
+                this.hitpoints += enemy.totalHitpoints * 0.25;
+                this.totalHitpoints += enemy.totalHitpoints * 0.1;
+            }
+        }
+    }
+}
+class Chempion extends Fighter {
+    defenceValue = false;
+    constructor(obj) {
+        super(obj);
     }
     heal() {
         this.hitpoints < this.totalHitpoints - 5 ? (this.hitpoints += 5) : console.log('Буде оверхіл, а це заборонено');
     }
-    defence = () => (this.d = true);
+    defence = () => (this.defenceValue = true);
 }
 class Monster extends Fighter {
-    #k = 0;
+    burst = 0;
     constructor(obj) {
         super(obj);
     }
-    fight(enemy) {
-        if (enemy === this) return "Ви б'єте самого себе";
-
-        if (enemy.d && enemy.__proto__.constructor.name === 'Chempion') {
-            enemy.totalHitpoints++;
-            enemy.d = false;
-            return 'Ваш опонент чемпіон і він блокував атаку';
-        }
-
-        if (enemy.isAlive() && this.isAlive()) {
-            this.#k === 2 || this.#k === 1 ? (enemy.hitpoints -= this.attack * 2) : (enemy.hitpoints -= this.attack);
-            this.#k--;
-        } else return 'Ваш опонент мертвий або ви';
-
-        if (!enemy.isAlive()) {
-            this.hitpoints += enemy.totalHitpoints * 0.25;
-            this.totalHitpoints += enemy.totalHitpoints * 0.1;
-        }
-    }
-    enrange = () => (this.#k = 2);
+    enrange = () => (this.burst = 2);
     fury() {
         if (this.hitpoints > 5) {
             this.totalHitpoints -= 5;
@@ -85,7 +77,6 @@ class Monster extends Fighter {
             console.log('Cacannot execute this method if there is not enough hitpoints');
         }
     }
-    test = () => this;
 }
 
 var hunter = new Chempion({ name: 'Rexxar', attack: 10, hitpoints: 60 });
